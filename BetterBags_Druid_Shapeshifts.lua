0076aa72d88d752443d45101a3020e02efcbec0b
ZehelFenris <43239740+ZehelFenris@users.noreply.github.com>
2023-12-28 05:09:53 +0100

    Add files via upload

---
+++ BetterBags_Druid_Shapeshifts.lua
@@ -0,0 +1,65 @@
+---@class BetterBags: AceAddon
+local addon = LibStub('AceAddon-3.0'):GetAddon("BetterBags")
+
+---@class Categories: AceModule
+local categories = addon:GetModule('Categories')
+
+---@class Localization: AceModule
+local L = addon:GetModule('Localization')
+
+--Aquatic
+categories:AddItemToCategory(210753, L:G("Druid shapeshifts")) --Scale of the Prismatic Whiskerfish
+categories:AddItemToCategory(40919, L:G("Druid shapeshifts")) --Glyph of the Orca
+categories:AddItemToCategory(162027, L:G("Druid shapeshifts")) --Glyph of the Tideskipper
+categories:AddItemToCategory(162022, L:G("Druid shapeshifts")) --Glyph of the Dolphin
+categories:AddItemToCategory(184097, L:G("Druid shapeshifts")) --Glyph of the Aquatic Chameleon
+
+--Cat
+categories:AddItemToCategory(210728, L:G("Druid shapeshifts")) --Moon-Blessed Claw
+categories:AddItemToCategory(210650, L:G("Druid shapeshifts")) --Mark of the Keen-Eyed Dreamsaber
+categories:AddItemToCategory(210755, L:G("Druid shapeshifts")) --Silent Mark of the Dreamsaber
+
+--Bear
+categories:AddItemToCategory(210751, L:G("Druid shapeshifts")) --Mark of the Hibernating Runebear
+categories:AddItemToCategory(210729, L:G("Druid shapeshifts")) --Mark of the Verdant Bristlebruin
+categories:AddItemToCategory(210727, L:G("Druid shapeshifts")) --Pollenfused Bristlebruin Fur Sample
+categories:AddItemToCategory(210766, L:G("Druid shapeshifts")) --Silent Mark of the Umbraclaw
+categories:AddItemToCategory(210739, L:G("Druid shapeshifts")) --Mark of the Snowy Umbraclaw
+categories:AddItemToCategory(210647, L:G("Druid shapeshifts")) --Mark of the Umbramane
+categories:AddItemToCategory(43334, L:G("Druid shapeshifts")) --Glyph of the Ursol Chameleon
+
+--Flight
+categories:AddItemToCategory(210754, L:G("Druid shapeshifts")) --Feather of the Blazing Somnowl
+categories:AddItemToCategory(210645, L:G("Druid shapeshifts")) --Feather of Friends
+categories:AddItemToCategory(210535, L:G("Druid shapeshifts")) --Mark of the Slumbering Somnowl
+categories:AddItemToCategory(162029, L:G("Druid shapeshifts")) --Glyph of the Humble Flyer
+categories:AddItemToCategory(129021, L:G("Druid shapeshifts")) --Glyph of the Sentinel
+categories:AddItemToCategory(187887, L:G("Druid shapeshifts")) --Mark of the Gloomstalker Dredbat
+categories:AddItemToCategory(187931, L:G("Druid shapeshifts")) --Mark of the Regal Dredbat
+categories:AddItemToCategory(187936, L:G("Druid shapeshifts")) --Mark of the Sable Ardenmoth
+categories:AddItemToCategory(187888, L:G("Druid shapeshifts")) --Mark of the Shimmering Ardenmoth
+categories:AddItemToCategory(187933, L:G("Druid shapeshifts")) --Mark of the Duskwing Raven
+categories:AddItemToCategory(184100, L:G("Druid shapeshifts")) --Glyph of the Aerial Chameleon
+
+--Moonkin
+categories:AddItemToCategory(211280, L:G("Druid shapeshifts")) --Feather of the Smoke Red Moon
+categories:AddItemToCategory(44922, L:G("Druid shapeshifts")) --Glyph of Stars
+categories:AddItemToCategory(211400, L:G("Druid shapeshifts")) --Glyph of the Lunar Chameleon
+
+--Travel
+categories:AddItemToCategory(210674, L:G("Druid shapeshifts")) --Mark of the Sable Dreamtalon
+categories:AddItemToCategory(211080, L:G("Druid shapeshifts")) --Mark of the Boreal Dreamtalon
+categories:AddItemToCategory(210683, L:G("Druid shapeshifts")) --Mark of the Dreamtalon Matriarch
+categories:AddItemToCategory(211081, L:G("Druid shapeshifts")) --Mark of the Auroral Dreamtalon
+categories:AddItemToCategory(210764, L:G("Druid shapeshifts")) --Silent Mark of the Dreamtalon
+categories:AddItemToCategory(210767, L:G("Druid shapeshifts")) --Silent Mark of the Dreamstag
+categories:AddItemToCategory(210736, L:G("Druid shapeshifts")) --Mark of the Smoldering Dreamstag
+categories:AddItemToCategory(89868, L:G("Druid shapeshifts")) --Glyph of the Cheetah
+categories:AddItemToCategory(140630, L:G("Druid shapeshifts")) --Glyph of the Doe
+categories:AddItemToCategory(187934, L:G("Druid shapeshifts")) --Mark of the Midnight Runestag
+categories:AddItemToCategory(187884, L:G("Druid shapeshifts")) --Mark of the Twilight Runestag
+categories:AddItemToCategory(184096, L:G("Druid shapeshifts")) --Glyph of the Swift Chameleon
+categories:AddItemToCategory(136789, L:G("Druid shapeshifts")) --Tome of the Wilds: Mount Form
+
+--Treant
+categories:AddItemToCategory(136787, L:G("Druid shapeshifts")) --Tome of the Wilds: Treant Form
